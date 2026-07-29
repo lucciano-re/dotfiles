74956f3b2e5bdffb6832db633427f825554af400
Lucciano <luciano.s.renda@gmail.com>
2026-07-28 22:24:26 -0300

    jippieees

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    opts.indent = opts.indent or {}
+    opts.indent.disable = opts.indent.disable or {}
+    table.insert(opts.indent.disable, "c")
+  end,
+}
